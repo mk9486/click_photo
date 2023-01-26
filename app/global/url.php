58cc54a1b4a4a4e7a5de4f90beec6a93420b8c10
Malith Madhushan <118029683+mk9486@users.noreply.github.com>
2023-01-26 06:43:24 +0530

    port issue fixed

--- app/global/url.php
+++ app/global/url.php
@@ -5,11 +5,11 @@
     define('PROTOCOL',(!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS'] == 'on')) ? 'https://' : 'http://');
     $G_parts = explode('/',$_SERVER['REQUEST_URI']);
     
-    $G_global_url = $_SERVER['SERVER_NAME'].":8080";
+    $G_global_url = $_SERVER['SERVER_NAME'];
     $documentRoot = $_SERVER['DOCUMENT_ROOT'];
     
     if(DOMAIN_START_PATH == 1){
-        $G_global_url = $_SERVER['SERVER_NAME'].':8080'.'/'.$G_parts[DOMAIN_START_PATH];
+        $G_global_url = $_SERVER['SERVER_NAME'].'/'.$G_parts[DOMAIN_START_PATH];
         $documentRoot = $_SERVER['DOCUMENT_ROOT'].'/'.$G_parts[DOMAIN_START_PATH];
     }
 
